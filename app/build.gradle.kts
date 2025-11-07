bc3a76e28038defc03390764d442bf919aa9531e
gowtham <gowthamon@facilio.com>
2025-11-08 00:27:39 +0530

    version increase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.cmp.pushuptracker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.0.6"
+        versionCode = 14
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
